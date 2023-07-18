511943882e1a41417a64d5caf8ca62c13d0dae7a
Nguyen Thai <tknguyen@linagora.com>
2023-07-18 17:50:15 +0700

    Change signing identity for debug and release

--- scripts/package-macos-debug.sh
+++ scripts/package-macos-debug.sh
@@ -5,4 +5,6 @@ echo "Installing appdmg."
 npm install -g appdmg
 
 echo "Packaging."
+# Change code signing identity
+sed -i .bak "s/@@IDENTITY@@/Apple Development: Nguyen Thai/" macos/packaging/dmg/make_config.yaml
 flutter pub global run flutter_distributor:main.dart package --platform macos --target dmg --skip-clean --flutter-build-args="profile"
