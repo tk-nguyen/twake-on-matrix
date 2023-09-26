1396aa2d208de47e7aad0f87d998b3ce4ac1a909
Nguyen Thai <tknguyen@linagora.com>
2023-09-26 15:05:15 +0700

    Use zip package instead of appimage

--- scripts/package-linux-debug.sh
+++ scripts/package-linux-debug.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-# Setup alias
-function cp() {
-    cp -f "$@"
-}
-export -f cp
-
-# Setup AppImageTool for packaging
-echo "Setting up AppImageTool"
-curl -o appimagetool -L "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
-chmod +x appimagetool
-sudo mv appimagetool /usr/local/bin/
-
 echo "Packaging."
-flutter pub global run flutter_distributor:main.dart package --platform linux --targets appimage --skip-clean --flutter-build-args="profile"
+flutter pub global run flutter_distributor:main.dart package --platform linux --targets zip --skip-clean --flutter-build-args="profile"
