c2a1225190459e5faa5ab8c1ca438726fc540208
Nguyen Thai <tknguyen@linagora.com>
2024-04-15 16:12:36 +0700

    Build app for public platform

--- scripts/build-android-debug.sh
+++ scripts/build-android-debug.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
 flutter build apk --debug
+cp build/app/outputs/apk/debug/app-debug.apk twake-on-matrix-debug.apk
--- scripts/build-ios-debug.sh
+++ scripts/build-ios-debug.sh
@@ -1,10 +1,11 @@
-#!/bin/sh -ve
-#
+#!/bin/bash
+
 # Have to run this first for dart-define
 flutter build ios --dart-define=REGISTRATION_URL="$REGISTRATION_URL" \
                   --dart-define=TWAKE_WORKPLACE_HOMESERVER="$TWAKE_WORKPLACE_HOMESERVER" \
                   --dart-define=PLATFORM="$PLATFORM" \
                   --dart-define=HOME_SERVER="$TWAKE_WORKPLACE_HOMESERVER" \
-                  --no-codesign --release
+                  --no-codesign --debug
 
-bundle exec fastlane beta
+bundle exec fastlane dev
+cp Runner.ipa ../Runner.ipa
