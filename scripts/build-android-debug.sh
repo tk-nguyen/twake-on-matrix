15603a6a6dc12421cb341106e2a734d75c85dbc6
Nguyen Thai <tknguyen@linagora.com>
2024-04-15 16:24:28 +0700

    Added missing android flags

--- scripts/build-android-debug.sh
+++ scripts/build-android-debug.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
-flutter build apk --debug
+flutter build apk --debug \
+                  --dart-define=REGISTRATION_URL="$REGISTRATION_URL" \
+                  --dart-define=TWAKE_WORKPLACE_HOMESERVER="$TWAKE_WORKPLACE_HOMESERVER" \
+                  --dart-define=PLATFORM="$PLATFORM" \
+                  --dart-define=HOME_SERVER="$TWAKE_WORKPLACE_HOMESERVER"
+
 cp build/app/outputs/apk/debug/app-debug.apk twake-on-matrix-debug.apk
--- scripts/release-playstore-beta.sh
+++ scripts/release-playstore-beta.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
-flutter build appbundle --release
+flutter build appbundle --release \
+                        --dart-define=REGISTRATION_URL="$REGISTRATION_URL" \
+                        --dart-define=TWAKE_WORKPLACE_HOMESERVER="$TWAKE_WORKPLACE_HOMESERVER" \
+                        --dart-define=PLATFORM="$PLATFORM" \
+                        --dart-define=HOME_SERVER="$TWAKE_WORKPLACE_HOMESERVER"
+
 cd android
 bundle exec fastlane deploy_internal_test
